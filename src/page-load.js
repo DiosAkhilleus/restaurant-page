9ab2f4ef086a25195e484681d499f34c3b29652b
DiosAkhilleus <abertin1809@gmail.com>
2021-02-28 01:51:07 -0600

    added footer nav & center picture

--- src/page-load.js
+++ src/page-load.js
@@ -8,8 +8,12 @@ const pageLoad = () => { //loads all html elements into the content div of index
 
     let main = document.createElement('div');
         let title = document.createElement('h1');
+        let img = document.createElement('img');
 
     let footer = document.createElement('div');
+        let jobs = document.createElement('a');
+        let services = document.createElement('a');
+
     
     header.setAttribute('id', 'header');
     main.setAttribute('id', 'main');
@@ -18,27 +22,42 @@ const pageLoad = () => { //loads all html elements into the content div of index
     title.setAttribute('id', 'title');
         title.innerHTML = 'Podarkes BBQ';
 
-    home.setAttribute('class', 'nav-list');
-    home.setAttribute('href', 'https://google.com');
+    img.setAttribute('src', 'https://jesspryles.com/wp-content/uploads/2020/12/pork-belly-sliders-1440x900.jpg')
+
+    home.setAttribute('class', 'header-nav');
+    home.setAttribute('href', '#');
         home.innerHTML = 'Home';
-    menu.setAttribute('class', 'nav-list');
+    menu.setAttribute('class', 'header-nav');
+    menu.setAttribute('href', '#');
         menu.innerHTML = 'Menu';
-    contact.setAttribute('class', 'nav-list');
+    contact.setAttribute('class', 'header-nav');
+    contact.setAttribute('href', '#');
         contact.innerHTML = 'Contact';
-    about.setAttribute('class', 'nav-list');
+    about.setAttribute('class', 'header-nav');
     about.setAttribute('id', 'about');
+    about.setAttribute('href', '#');
         about.innerHTML = 'About';
+
+    jobs.setAttribute('class', 'footer-nav');
+        jobs.innerHTML = 'Jobs';
+    services.setAttribute('class', 'footer-nav');
+        services.innerHTML = 'Services';
 
     content.appendChild(header);
     header.after(main);
     main.after(footer);
 
     main.appendChild(title);
+    title.after(img);
 
     header.appendChild(home);
     home.after(menu);
     menu.after(contact);
     contact.after(about);
+
+    footer.appendChild(jobs);
+    jobs.after(services);
+    
 
     console.log("pageLoad?");
 }
