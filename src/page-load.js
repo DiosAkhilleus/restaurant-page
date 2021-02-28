ff01633a8b774d3c2fa48d15aedd3388d3d3e6f2
DiosAkhilleus <abertin1809@gmail.com>
2021-02-28 01:06:51 -0600

    added more elements, styled page a little

--- src/page-load.js
+++ src/page-load.js
@@ -1,29 +1,39 @@
-const pageLoad = () => {
+const pageLoad = () => { //loads all html elements into the content div of index.html
     let content = document.getElementById('content');
     let header = document.createElement('nav');
-    let home = document.createElement('li');
-    let menu = document.createElement('li');
-    let contact = document.createElement('li');
-    let about = document.createElement('li');
+        let home = document.createElement('a');
+        let menu = document.createElement('a');
+        let contact = document.createElement('a');
+        let about = document.createElement('a');
+
     let main = document.createElement('div');
+        let title = document.createElement('h1');
+
     let footer = document.createElement('div');
+    
     header.setAttribute('id', 'header');
     main.setAttribute('id', 'main');
     footer.setAttribute('id', 'footer');
 
+    title.setAttribute('id', 'title');
+        title.innerHTML = 'Podarkes BBQ';
+
     home.setAttribute('class', 'nav-list');
-    home.innerHTML = 'Home';
+    home.setAttribute('href', 'https://google.com');
+        home.innerHTML = 'Home';
     menu.setAttribute('class', 'nav-list');
-    menu.innerHTML = 'Menu';
+        menu.innerHTML = 'Menu';
     contact.setAttribute('class', 'nav-list');
-    contact.innerHTML = 'Contact';
+        contact.innerHTML = 'Contact';
     about.setAttribute('class', 'nav-list');
     about.setAttribute('id', 'about');
-    about.innerHTML = 'About';
+        about.innerHTML = 'About';
 
     content.appendChild(header);
     header.after(main);
     main.after(footer);
+
+    main.appendChild(title);
 
     header.appendChild(home);
     home.after(menu);
